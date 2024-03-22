81e0a5fe24b92e4e385ecd467d7427d80ad373cb
Salomon BRYS <salomon.brys@gmail.com>
2024-03-22 16:48:43 +0100

    Fix for targeting both M2 & M3

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,3 +6,9 @@ allprojects {
     group = "org.kodein.emoji"
     version = "1.3.0"
 }
+
+allprojects {
+    tasks.configureEach {
+        if (name == "kotlinStoreYarnLock") enabled = false
+    }
+}
