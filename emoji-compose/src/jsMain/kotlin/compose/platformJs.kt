46fb7c55aded3f4570005ab9eaccd5f6370e3d57
Romain BOISSELLE <romain.boisselle@proton.me>
2026-07-19 10:46:52 +0200

    chore: bump to latest deps

--- emoji-compose/src/jsMain/kotlin/compose/platformJs.kt
+++ emoji-compose/src/jsMain/kotlin/compose/platformJs.kt
@@ -5,12 +5,13 @@ import kotlinx.coroutines.await
 import org.khronos.webgl.ArrayBuffer
 import org.khronos.webgl.Int8Array
 import org.khronos.webgl.get
+import org.w3c.fetch.RequestInit
 import org.w3c.fetch.Response
 
 
 @OptIn(ExperimentalWasmJsInterop::class)
 internal actual suspend fun platformDownloadBytes(url: String): ByteArray {
-    val response = window.fetch(url).await<Response>()
+    val response = window.fetch(url, RequestInit()).await<Response>()
     val bufferPromise = response.arrayBuffer()
     val buffer = bufferPromise.await<ArrayBuffer>()
     val array = Int8Array(buffer)
